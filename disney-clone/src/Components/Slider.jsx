30d9375fc528b3771297c726a3486501a91d27b1
Muadeel56 <muadeel59@gmail.com>
2024-09-09 22:04:32 +0500

    Complete the slider

--- disney-clone/src/Components/Slider.jsx
+++ disney-clone/src/Components/Slider.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import GlobalApi from '../Services/GlobalApi'
 import { HiChevronLeft } from "react-icons/hi2";
 import { HiChevronRight } from "react-icons/hi2";
 
 
+const screenWidth = window.innerWidth
+
 const ImageBaseUrl = "https://image.tmdb.org/t/p/w500"
 function Slider() {
   const [movieList, setMovieList] = useState([])
+  const elementRef = useRef()
     useEffect(() => {
         getTrendingMovies()
     },[ ])
@@ -17,15 +20,23 @@ function Slider() {
             setMovieList(resp.data.results)
         })
     }
+
+    const sliderRight = (element) => {
+      element.scrollLeft +=screenWidth-110
+    }
+
+    const sliderLeft = (element) => {
+      element.scrollLeft -=screenWidth-110
+    }
   return (
     <div>
-      <HiChevronLeft className='hidden md:block text-white text-[30px] absolute mt-[150px] mx-8 cursor-pointer'/>
-      <HiChevronRight className='hidden md:block text-white text-[30px] absolute right-0 mt-[150px] mx-8 cursor-pointer'/>
+      <HiChevronLeft className='hidden md:block text-white text-[30px] absolute mt-[150px] mx-8 cursor-pointer' onClick={()=> sliderLeft(elementRef.current)}/>
+      <HiChevronRight className='hidden md:block text-white text-[30px] absolute right-0 mt-[150px] mx-8 cursor-pointer' onClick={()=> sliderRight(elementRef.current)}/>
 
-    <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-none'>
+    <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-none scroll-smooth' ref={elementRef}>
       {movieList.map((item,index) => (
-        <img src={`${ImageBaseUrl}${item.backdrop_path}`}
-         className='h-[310px] min-w-full object-cover object-left-top mr-10 rounded-md' alt=""  />
+        <img src={`${ImageBaseUrl}${item.poster_path}`}
+         className='md:h-[300px] object-cover object-left-top mr-10 rounded-md hover:border-2 hover:border-gray-500' alt=""  />
       ))}
     </div>
     </div>
