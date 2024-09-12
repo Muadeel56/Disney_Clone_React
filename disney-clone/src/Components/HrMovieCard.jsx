8d122dbcb7b1d934d68502a1e6ee0835d3212c8a
Muadeel56 <muadeel59@gmail.com>
2024-09-12 23:40:41 +0500

    Styling HrMovieCard

--- disney-clone/src/Components/HrMovieCard.jsx
+++ disney-clone/src/Components/HrMovieCard.jsx
@@ -3,14 +3,14 @@ const ImageBaseUrl = "https://image.tmdb.org/t/p/w500"
 
 function HrMovieCard({movie}) {
   return (
-    <section>
+    <section className="hover:scale-100 transition-all duration-75 ease-in cursor-pointer">
       <img
         src={`${ImageBaseUrl}${movie.backdrop_path}`}
         alt=""
-        className="w-[110px] md:w-[200px] rounded-lg hover:border-[3px] hover:border-gray-400
+        className="w-[110px] md:w-[260px] rounded-lg hover:border-[3px] hover:border-gray-400
         hover:scale-110 transition-all duration-75 ease-in cursor-pointer"
       />
-      <h2 className="w-[110px] md:w-[200px] text-white mt-2">{movie.title}</h2>
+      <h2 className="w-[110px] md:w-[260px] text-white mt-2">{movie.title}</h2>
     </section>
   );
 }
